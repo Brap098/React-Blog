d20678688e96312ffa40ea7f881c52bf7b5a690b
Brap098 <Brapagon.Work@gmail.com>
2022-09-06 20:06:05 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="navbar">
+      <div className="link">
+        <a className="Main">Main Page</a>
+        <a className="Post">Post Page</a>
+      </div>
+    </div>
+  );
+}
+
+export default App;
