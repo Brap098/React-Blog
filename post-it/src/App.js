427211ec2a60408d10bf86a09c8f42ecc849b8ba
Brap098 <Brapagon.Work@gmail.com>
2022-09-08 20:04:36 -0400

    css line page etc.

--- post-it/src/App.js
+++ post-it/src/App.js
@@ -2,15 +2,22 @@ import './App.css';
 
 function App() {
   return (
+  <div id="paper">
+  <div id="pattern">
+  <div id="content">
+
     <div className="navbar">
       <div className="link">
         <a className="Main">Main Page</a>
         <a className="Post">Post Page</a>
+        <a className="About">About</a>
       </div>
       <input type="text" placeholder="ID please"></input>
       <input type="text" placeholder="Password"></input>
     </div>
-    
+    </div>
+    </div>
+    </div>
   );
 }
 
